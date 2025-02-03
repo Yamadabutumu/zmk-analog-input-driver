9fdd0c7e0a4c174d1a24dee4233dcfe34df1f472
Yamada_butumu <95560415+Yamadabutumu@users.noreply.github.com>
2025-02-03 15:32:13 +0900

    Update analog_input.c

--- src/analog_input.c
+++ src/analog_input.c
@@ -218,7 +218,8 @@ static int analog_input_report_data(const struct device *dev) {
 
 	if (raw < 20){
 		if (ch_cfg.adc_channel.channel_id == 0){
-            		zmk_hid_press(N0);  // アクション関数を呼び出す
+            		zmk_hid_keyboard_press(N0);  // アクション関数を呼び出す
+			zmk_hid_keyboard_release(N0);
 			k_msleep(50);
 		}
         }
